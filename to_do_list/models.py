8131b7e7227f8408fb0f8803f5ca3791a332fa35
Sergei Alabugin <sergei_alabugin@mail.ru>
2017-08-04 15:16:26 +0600

    Create models

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Task(models.Model):
+    text = models.CharField(max_length=200)
+    deadline = models.DateTimeField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
