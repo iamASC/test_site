cfb74484a1561e2aadbd79309be4744ce01b08f1
Sergei Alabugin <sergei_alabugin@mail.ru>
2017-08-05 13:35:51 +0600

    Replace 3 views by one

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from to_do_list import views
 
 urlpatterns = [
-    url(r'^todaytasks/$', views.todaytasks, name = 'todaytasks'),
-    url(r'^weektasks/$', views.weektasks, name = 'weektasks'),
-    url(r'^overtasks/$', views.overtasks, name = 'overtasks'),
+    url(r'^(todaytasks)/$', views.task_list, name = 'todaytasks'),
+    url(r'^(weektasks)/$', views.task_list, name = 'weektasks'),
+    url(r'^(overtasks)/$', views.task_list, name = 'overtasks'),
 
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -4,22 +4,18 @@ import datetime
 from django.http import HttpResponse
 # Create your views here.
 
-def todaytasks(request):
-    today = Task.objects.filter(deadline=datetime.date.today())
-    return render(request, 'to_do_list/task_list.html',{'title':"Today's Tasks", 'list':today})
+title_dict ={
+'todaytasks':"Today's Tasks",
+'weektasks':"Week's Tasks",
+'overtasks': "Overdue Tasks",
+}
 
-
-def weektasks(request):
-    week = Task.objects.filter(deadline__range=(datetime.date.today(), datetime.datetime.today() + datetime.timedelta(days = 7)))
-    res = ''
-    for task in week:
-        res += task.category.name + ":" + task.text + ":" + str(task.deadline) + "<br>"
-    return HttpResponse(res)
-
-def overtasks(request):
-    over = Task.objects.filter(deadline__lt= datetime.datetime.today())
-    res = ''
-    for task in over:
-        res += task.category.name + ":" + task.text + ":" + str(task.deadline) + "<br>"
-    return HttpResponse(res)
+def task_list(request, url_arg):
+    if url_arg == 'todaytasks':
+        list = Task.objects.filter(deadline=datetime.date.today())
+    elif url_arg == 'weektasks':
+        list = Task.objects.filter(deadline__range=( datetime.date.today(), datetime.date.today() + datetime.timedelta(days = 7)))
+    elif url_arg == 'overtasks':
+        list = Task.objects.filter(deadline__lt= datetime.date.today())
+    return render(request, 'to_do_list/task_list.html',{'title':title_dict[url_arg], 'list':list})
 
